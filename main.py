82f5274fb66fe96eaceff87feaff82d4a9e1724e
vvcdev <51450950+vvcdev@users.noreply.github.com>
2023-12-15 12:15:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, jsonify, request
+from duckpy import Client
 
 app = Flask(__name__)
 
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query', '')  # Get the search parameter from the URL
+    if not query:
+        return jsonify({"error": "Empty search query"}), 400
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    try:
+        client = Client()
+        results = client.search(query)
+        # Transform results into JSON-friendly format
+        search_results = []
+        for result in results:
+            search_results.append({
+                'title': result.title,
+                'url': result.url,
+                'description': result.description
+            })
 
+        return jsonify(search_results)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    # Change host to '0.0.0.0' to make the app accessible from any network interface
+    app.run(debug=True, host='0.0.0.0')
